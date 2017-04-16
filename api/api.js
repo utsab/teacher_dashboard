7fff466b8e6e28e3ec9c9137811210f40ab854fd
Utsab Saha <utsab.k.saha@gmail.com>
2017-04-16 15:47:42 -0700

    Start server after MongoDB connectoin succeeds; Fetch users collection from db

--- api/api.js
+++ api/api.js
@@ -25,89 +25,98 @@ function connectToDB() {
   MongoClient.connect('mongodb://psaigal:password@ds161630.mlab.com:61630/fcc-test-db', (err, database) => {
   if (err) return console.log(err)
   db = database; 
-  // app.listen(3000, () => {
-  //   console.log('listening on 3000')
-  // })
 
   console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$   we are connected to db!"); 
-  console.log(db); 
+  var users = db.collection('users').find().toArray(function(err, results) {
+    console.log("Showing users: "); 
+    console.log(results)
+    // send HTML file populated with quotes here
+  }); 
 
-  // 
+  setupServer(); 
+
+
+
   })
 }
 
 connectToDB(); 
 
-app.use(session({
-  secret: 'react and redux rule!!!!',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 60000 }
-}));
-app.use(bodyParser.json());
+function setupServer() {
+
+  console.log("In setupServer... ********************")
+
+  app.use(session({
+    secret: 'react and redux rule!!!!',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 }
+  }));
+  app.use(bodyParser.json());
 
 
-app.use((req, res) => {
-  const splittedUrlPath = req.url.split('?')[0].split('/').slice(1);
+  app.use((req, res) => {
+    const splittedUrlPath = req.url.split('?')[0].split('/').slice(1);
 
-  const {action, params} = mapUrl(actions, splittedUrlPath);
+    const {action, params} = mapUrl(actions, splittedUrlPath);
 
-  if (action) {
-    action(req, params)
-      .then((result) => {
-        if (result instanceof Function) {
-          result(res);
-        } else {
-          res.json(result);
-        }
-      }, (reason) => {
-        if (reason && reason.redirect) {
-          res.redirect(reason.redirect);
-        } else {
-          console.error('API ERROR:', pretty.render(reason));
-          res.status(reason.status || 500).json(reason);
+    if (action) {
+      action(req, params)
+        .then((result) => {
+          if (result instanceof Function) {
+            result(res);
+          } else {
+            res.json(result);
+          }
+        }, (reason) => {
+          if (reason && reason.redirect) {
+            res.redirect(reason.redirect);
+          } else {
+            console.error('API ERROR:', pretty.render(reason));
+            res.status(reason.status || 500).json(reason);
+          }
+        });
+    } else {
+      res.status(404).end('NOT FOUND');
+    }
+  });
+
+
+  const bufferSize = 100;
+  const messageBuffer = new Array(bufferSize);
+  let messageIndex = 0;
+
+  if (config.apiPort) {
+    const runnable = app.listen(config.apiPort, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.info('----\n==> 🌎  API is running on port %s', config.apiPort);
+      console.info('==> 💻  Send requests to http://%s:%s', config.apiHost, config.apiPort);
+    });
+
+    io.on('connection', (socket) => {
+      socket.emit('news', {msg: `'Hello World!' from server`});
+
+      socket.on('history', () => {
+        for (let index = 0; index < bufferSize; index++) {
+          const msgNo = (messageIndex + index) % bufferSize;
+          const msg = messageBuffer[msgNo];
+          if (msg) {
+            socket.emit('msg', msg);
+          }
         }
       });
+
+      socket.on('msg', (data) => {
+        data.id = messageIndex;
+        messageBuffer[messageIndex % bufferSize] = data;
+        messageIndex++;
+        io.emit('msg', data);
+      });
+    });
+    io.listen(runnable);
   } else {
-    res.status(404).end('NOT FOUND');
+    console.error('==>     ERROR: No PORT environment variable has been specified');
   }
-});
-
-
-const bufferSize = 100;
-const messageBuffer = new Array(bufferSize);
-let messageIndex = 0;
-
-if (config.apiPort) {
-  const runnable = app.listen(config.apiPort, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.info('----\n==> 🌎  API is running on port %s', config.apiPort);
-    console.info('==> 💻  Send requests to http://%s:%s', config.apiHost, config.apiPort);
-  });
-
-  io.on('connection', (socket) => {
-    socket.emit('news', {msg: `'Hello World!' from server`});
-
-    socket.on('history', () => {
-      for (let index = 0; index < bufferSize; index++) {
-        const msgNo = (messageIndex + index) % bufferSize;
-        const msg = messageBuffer[msgNo];
-        if (msg) {
-          socket.emit('msg', msg);
-        }
-      }
-    });
-
-    socket.on('msg', (data) => {
-      data.id = messageIndex;
-      messageBuffer[messageIndex % bufferSize] = data;
-      messageIndex++;
-      io.emit('msg', data);
-    });
-  });
-  io.listen(runnable);
-} else {
-  console.error('==>     ERROR: No PORT environment variable has been specified');
 }
