5ba8c39698718c13ba46cbe956878c6a8eb0b957
Tom Lee <tlee418@g.ucla.edu>
2017-04-23 08:27:47 -0700

    Can't pipe data thru front end
    * Following pattern in Chat.js

---
+++ src/containers/Challenge/Challenge.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Challenge extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Testing</h1>
+      </div>
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ import {
     Survey,
     NotFound,
     Pagination,
+    Challenge,
   } from 'containers';
 
 export default (store) => {
@@ -52,6 +53,7 @@ export default (store) => {
       <Route path="pagination" component={Pagination}/>
       <Route path="survey" component={Survey}/>
       <Route path="widgets" component={Widgets}/>
+      <Route path="challenge" component={Challenge}/>
 
       { /* Catch all route */ }
       <Route path="*" component={NotFound} status={404} />
