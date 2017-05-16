5d9b990fd5de921ba0cfd6ddcbbdb566590b7bca
psaigal <psaigal48@gmail.com>
2017-05-16 16:06:05 -0700

    Add two navbars

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -80,63 +80,73 @@ export default class App extends Component {
     const styles = require('./App.scss');
 
     return (
-      <div className={styles.app}>
-        <Helmet {...config.app.head}/>
-        <Navbar fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLink to="/">
-                <a href="#" className="navbar-brand"><img className="img-responsive" src={logo}/></a>
-                <p>Teacher Dashboard BETA</p>
-              </IndexLink>
-            </Navbar.Brand>
-            <Navbar.Toggle/>
-          </Navbar.Header>
-
-          {user &&
-            <Navbar.Collapse eventKey={0}>
-              <Nav navbar>
-                {user && <LinkContainer to="/chat">
-                  <NavItem eventKey={1}>Chat</NavItem>
-                </LinkContainer>}
-                {user && <LinkContainer to="/manageClass">
-                  <NavItem eventKey={1}>Manage Class</NavItem>
-                </LinkContainer>}
-                {user && <LinkContainer to="/widgets">
-                  <NavItem eventKey={2}>Widgets</NavItem>
-                </LinkContainer>}
-                {user && <LinkContainer to="/survey">
-                  <NavItem eventKey={3}>Survey</NavItem>
-                </LinkContainer>}
-                {user && <LinkContainer to="/pagination">
-                  <NavItem eventKey={4}>Pagination</NavItem>
-                </LinkContainer>}
-                {user && <LinkContainer to="/about">
-                  <NavItem eventKey={5}>About Us</NavItem>
-                </LinkContainer>}
-                {user &&
-                <LinkContainer to="/logout">
-                  <NavItem eventKey={6} className="logout-link" onClick={this.handleLogout}>
-                    Logout
-                  </NavItem>
-                </LinkContainer>}
-              </Nav>
-              {user &&
-              <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
+      <div>
+        <div className={styles.app}>
+          <Helmet {...config.app.head}/>
+          <Navbar fixedTop>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <IndexLink to="/">
+                  <a href="#" className="navbar-brand"><img className="img-responsive" src={logo}/></a>
+                </IndexLink>
+              </Navbar.Brand>
+              <Navbar.Text pullLeft>
+                Teacher Dashboard BETA
+              </Navbar.Text>
+               {user && <Navbar.Toggle />}
+            </Navbar.Header>
+             {user && <Navbar.Collapse>
+              <Navbar.Text pullRight>
+                <Navbar.Link className="logout-link" onClick={this.handleLogout} href="/logout">Logout</Navbar.Link>
+              </Navbar.Text>
             </Navbar.Collapse>}
-        </Navbar>
+          </Navbar>
+          <div className={styles.appNav}>
+            {user && <Navbar>
+              <Navbar.Header>
+                <Navbar.Brand>
+                </Navbar.Brand>
+                <Navbar.Toggle/>
+              </Navbar.Header>
+                <Navbar.Collapse eventKey={0}>
+                  <Nav navbar>
+                    {user && <LinkContainer to="/chat">
+                      <NavItem eventKey={1}>Chat</NavItem>
+                    </LinkContainer>}
+                    {user && <LinkContainer to="/manageClass">
+                      <NavItem eventKey={1}>Manage Class</NavItem>
+                    </LinkContainer>}
+                    {user && <LinkContainer to="/widgets">
+                      <NavItem eventKey={2}>Widgets</NavItem>
+                    </LinkContainer>}
+                    {user && <LinkContainer to="/survey">
+                      <NavItem eventKey={3}>Survey</NavItem>
+                    </LinkContainer>}
+                    {user && <LinkContainer to="/pagination">
+                      <NavItem eventKey={4}>Pagination</NavItem>
+                    </LinkContainer>}
+                    {user && <LinkContainer to="/about">
+                      <NavItem eventKey={5}>About Us</NavItem>
+                    </LinkContainer>}
+                  </Nav>
+                  {user &&
+                  <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
+                </Navbar.Collapse>
+            </Navbar>}
+          </div>
 
-        <div className={styles.appContent}>
-          {this.props.children}
-        </div>
-        <ShowUsers/>
-        <InfoBar/>
+          <div className={styles.appContent}>
+            {this.props.children}
+          </div>
+          <ShowUsers/>
+          <InfoBar/>
 
-        <div className="well text-center">
-          Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
+          <div className="well text-center">
+            Have questions? Ask for help <a
+            href="https://github.com/erikras/react-redux-universal-hot-example/issues"
+            target="_blank">on Github</a> or in the <a
+            href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
+          </div>
         </div>
       </div>
     );
