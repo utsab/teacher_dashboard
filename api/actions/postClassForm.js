bd200b27b4ee7c8fd34e6f1a8b7d16f2d80a192f
Utsab Saha <utsab.k.saha@gmail.com>
2017-06-28 20:23:47 -0700

    Validate github username when adding a new user

--- api/actions/postClassForm.js
+++ api/actions/postClassForm.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'); //mongo connection
+var http = require('http');
 
 	export default function postClassForm(req, err, model) {
 		return new Promise((resolve,reject) => {
@@ -19,6 +20,7 @@ var mongoose = require('mongoose'); //mongo connection
 	 		}
 
 
+
 	 		if ( errors.length > 0 ) {
 	 			console.log("Found " + errors.length + " form validation error(s)"); 
 	 			throw errors;
@@ -26,47 +28,94 @@ var mongoose = require('mongoose'); //mongo connection
 	 		}
 	 		
 
-	 		var Student = model.students;
-	 		var Teacher = model.teachers;
-
-	 		const teacherEmail = req.session.user.email;
-
-
-			const student = {
-			    email: req.body.email,
-			    github: req.body.github,
-			    firstname: req.body.firstname,
-		        lastname: req.body.lastname,
-		        notes: req.body.notes
-			};
-
-
-			Teacher.findOne({ 'email': teacherEmail }, function (err, person) {
+	 		var options = {
+	 		  host: 'fcc-profile-scraper.herokuapp.com',
+	 		  port: 80,
+	 		  path: '/user/' + req.body.github
+	 		};
 
 
 
-				Student.create({ firstName: student.firstname, lastName: student.lastname, githubUsername: student.github, email: student.email, notes: student.notes, teacher: person._id }, function (err, createdUser) {
-					    if(err) {
-							throw err;
-							reject();
-						}
-						else {
-							Student
-							.findOne({ 'email': student.email })
-							.populate('teacher') //This populates the author id with actual author information!
-							.exec(function (err, student) {
-							  if (err) return handleError(err);
-							});
+	 		http.get(options, function(resp){
+	 		  resp.setEncoding('utf8');
 
-							Student
-							.find({ teacher : person._id })
-							.exec(function (err, students) {
-							  if (err) return handleError(err);
-							  const allStudentsList = students;
-							  resolve(allStudentsList); 
-							});
-						}
+	 		  console.log("response status code: " + resp.statusCode);
+
+
+	 		  resp.on('data', function(chunk){
+	 		    //do something with chunk?  Maybe we can remove this 'data' handler
+	 		    console.log("received data!");
+	 		  });
+
+	 		  resp.on('end', function() {
+	 		  	console.log("resp ended!!!!!!"); 
+	 		  	console.log("status code in resp: " + this.statusCode); 
+
+	 		  	if (this.statusCode === 404) {
+	 		  		 errors.push("Github username is invalid"); 
+	 		  		 console.log("Found " + errors.length + " form validation error(s)"); 
+	 		  		 reject(errors);
+	 		  	} else {
+	 		  		// Assume request succeeded
+	 		  		console.log("Github is valid!...."); 
+	 		  		saveNewStudentToDB(req); 
+	 		  	}
+	 		  });
+
+
+	 		}).on("error", function(e){
+	 		  console.log("Got error:^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ " + e.message);
+	 		});
+
+
+
+	 		function saveNewStudentToDB(req) {
+	 			console.log("in saveNewStudentToDB........"); 
+
+		 		var Student = model.students;
+		 		var Teacher = model.teachers;
+
+		 		const teacherEmail = req.session.user.email;
+
+
+				const student = {
+				    email: req.body.email,
+				    github: "utsab",
+				    firstname: req.body.firstname,
+			        lastname: req.body.lastname,
+			        notes: req.body.notes
+				};
+
+
+				Teacher.findOne({ 'email': teacherEmail }, function (err, person) {
+
+
+
+					Student.create({ firstName: student.firstname, lastName: student.lastname, githubUsername: student.github, email: student.email, notes: student.notes, teacher: person._id }, function (err, createdUser) {
+						    if(err) {
+								throw err;
+								reject();
+							}
+							else {
+								Student
+								.findOne({ 'email': student.email })
+								.populate('teacher') //This populates the author id with actual author information!
+								.exec(function (err, student) {
+								  if (err) return handleError(err);
+								});
+
+								Student
+								.find({ teacher : person._id })
+								.exec(function (err, students) {
+								  if (err) return handleError(err);
+								  const allStudentsList = students;
+								  resolve(allStudentsList); 
+								});
+							}
 					});
 				});
+
+	 		}
+	 		
 	  	});
 	}
