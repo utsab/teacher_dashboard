37283f356bb4ae5e1fa2106ccf62ad8ba23dad33
Tom Lee <user512@users.noreply.github.com>
2017-07-02 12:02:58 -0700

    Remove Have questions? div

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -139,13 +139,6 @@ export default class App extends Component {
             {this.props.children}
           </div>
           <ShowUsers/>
-
-          <div className="well text-center">
-            Have questions? Ask for help <a
-            href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-            target="_blank">on Github</a> or in the <a
-            href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
-          </div>
         </div>
       </div>
     );
